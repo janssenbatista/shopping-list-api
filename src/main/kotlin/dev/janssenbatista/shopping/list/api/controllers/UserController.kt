44edfc55ec5b319cdd10820d49ecc0c45e584969
Janssen Batista <batistajanssen.dev@gmail.com>
2023-05-30 08:41:35 -0300

    update user controller

--- src/main/kotlin/dev/janssenbatista/shopping/list/api/controllers/UserController.kt
+++ src/main/kotlin/dev/janssenbatista/shopping/list/api/controllers/UserController.kt
@@ -2,7 +2,6 @@ package dev.janssenbatista.shopping.list.api.controllers
 
 import dev.janssenbatista.shopping.list.api.models.User
 import dev.janssenbatista.shopping.list.api.models.dtos.UserDTO
-import dev.janssenbatista.shopping.list.api.models.dtos.UserResponseDTO
 import dev.janssenbatista.shopping.list.api.services.UserService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -16,29 +15,28 @@ import java.util.*
 class UserController(private val userService: UserService) {
 
     @PostMapping
-    fun createUser(@RequestBody @Valid userDTO: UserDTO): ResponseEntity<Any> {
+    fun createUser(@RequestBody @Valid userDTO: UserDTO): ResponseEntity<User> {
         val createdUser = userService.save(User(email = userDTO.email, userPassword = userDTO.password))
-        val userResponseDTO = UserResponseDTO(createdUser.id, createdUser.email)
-        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser)
     }
 
     @GetMapping("/{id}")
     fun getUserById(@PathVariable(value = "id") userId: UUID, authentication: Authentication): ResponseEntity<User> {
-        if (getUser(authentication).id != userId)
+        val user = userService.findUserById(id = userId)
+        if (getUser(authentication).email != user.email)
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
-        return ResponseEntity.ok(userService.findUserById(userId))
+        return ResponseEntity.ok(user)
     }
 
     @PutMapping("/{id}")
     fun updateUser(@PathVariable(value = "id") userId: UUID,
                    @RequestBody @Valid userDTO: UserDTO,
-                   authentication: Authentication): ResponseEntity<Any> {
+                   authentication: Authentication): ResponseEntity<User> {
         if (getUser(authentication).id != userId)
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
         val updatedUser = userService.update(id = userId,
                 user = User(email = userDTO.email, userPassword = userDTO.password))
-        val userResponseDTO = UserResponseDTO(updatedUser.id, updatedUser.email)
-        return ResponseEntity.ok(userResponseDTO)
+        return ResponseEntity.ok(updatedUser)
     }
 
     @DeleteMapping("/{id}")
